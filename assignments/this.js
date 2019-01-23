d794e472d037b9498adf027e8db0aa74586339aa
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-01-23 16:11:20 -0500

    this is hard to explain lol

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,14 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+/* The four principles of "this";
+* in your own words. explain the four principled for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. global
+* * Global refers to the global object, in browsers, this is the window object
+* 2. implicit
+* * 
+* 3. explicit
+* *
+* 4. new
+* * 
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +16,7 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this === window);
 
 // Principle 2
 
